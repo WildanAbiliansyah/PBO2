aea069ee98c99bbe3eca0779a59c5a8ed3a577a1
WildanAbiliansyah <127711096+WildanAbiliansyah@users.noreply.github.com>
2023-06-26 20:33:51 +0700

    Add files via upload

---
+++ PRAKTIKUM 7/meta programing luas permukaan dan volume bola.py
@@ -0,0 +1,21 @@
+#Nama   : Mohammad Wildan Abiliansyah
+#Kelas  : R1
+#NIM    : 210511022
+
+class BolaMeta(type):
+    def _init_(cls, name, bases, attrs):
+        super()._init_(name, bases, attrs)
+        def luaspermukaan(cls, jari):
+            return 4 * 3.14 * jari * jari
+        cls.luaspermukaan = classmethod(luaspermukaan)
+
+        def volume(cls, jari):
+            return 4/3 * 3.14 * jari * jari * jari
+        cls.volume = classmethod(volume)
+class Luaspermukaandanvolume(metaclass=BolaMeta):
+    pass
+A = Luaspermukaandanvolume()
+B = A.luaspermukaan(21)
+C = A.volume(21)
+print('Luas Permukaan Bola:',B)
+print('Volume Bola:',C)
